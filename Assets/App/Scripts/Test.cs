aae7311884cdc665ae2651f99be5d51cb47a1832
Eder_Work <edermtz.04@gmail.com>
2015-03-13 02:58:47 -0600

    commit inicial

---
+++ Assets/App/Scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Controller c;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
