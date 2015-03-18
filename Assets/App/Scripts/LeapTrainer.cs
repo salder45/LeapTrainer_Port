b2344c364cdc250ce1e52365ba11685a1174e05a
Eder_Work <edermtz.04@gmail.com>
2015-03-17 21:56:54 -0600

    cambios varios

--- Assets/App/Scripts/LeapTrainer.cs
+++ Assets/App/Scripts/LeapTrainer.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class LeapTrainer : MonoBehaviour {
-
+	/*
+	 * It will be done using the RecordingControls class and extract the alghoritm from leaptrainer.js or some of alghoritms from papers
+	 */
 	// Use this for initialization
 	void Start () {
 	
